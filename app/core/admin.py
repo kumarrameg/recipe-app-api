fe42cb6b820318078fa5e412712d621416a811dd
Ramkumar <kumarrameg@gmail.com>
2023-02-10 08:46:11 +0530

    custom admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,36 @@
-from django.contrib import admin 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+
+  ordering = ['id']
+  list_display = ['email','name']
+  fieldsets = (
+    (None,{'fields': ('email', 'passoword')}),
+    (
+      _('Permissions'),
+      {
+        'fields':(
+          'is_active',
+          'is_staff',
+          'is_superuser',
+        )
+      }
+    ),
+    (
+      _('Importan dates'),
+      {
+        'fields': (
+          'last_login',
+          )
+      }
+    )
+  )
+
+  readonly_fields = ['last_login']
+
+
+admin.site.register(models.User,UserAdmin)
+
--- app/core/models.py
+++ app/core/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
     if not email:
       raise ValueError('The Email must be set')
     user = self.model(email=self.normalize_email(email),**extra_fields)
-    user.set_password = (password)
+    user.set_password(password)
     user.save(using=self._db)
 
     return user
